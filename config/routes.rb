da682dfbbc6c888d6a3e45938dcb08be4b6cf28c
Sundy Yaputra <sundy.yaputra@mokapos.com>
2019-08-22 21:57:41 +0800

    Removing dead routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :access_tokens
-    resources :app_fields
-    resources :app_settings
-    resources :apps 
-    resources :client_trusted_domains
-    resources :clients 
-    resources :permissions
-    resources :permissions_apps
-    resources :permissions_roles
-    resources :permissions_users
     resources :posts
     resources :posts_tags
-    resources :refresh_tokens
-    resources :roles 
-    resources :roles_users
-    resources :settings 
-    resources :subscribers
     resources :tags 
     resources :users  
   end
